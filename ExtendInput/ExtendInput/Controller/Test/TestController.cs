56f7182007b2798e7daaca85752d29aca2757450
John 'Nielk1' Klein <development@nielk1.com>
2022-11-19 22:55:19 -0600

    M button

--- ExtendInput/ExtendInput/Controller/Test/TestController.cs
+++ ExtendInput/ExtendInput/Controller/Test/TestController.cs
@@ -118,8 +118,9 @@ namespace ExtendInput.Controller.Test
 
             State.Controls["m1"] = new ControlButton();
             State.Controls["m2"] = new ControlButton();
-            State.Controls["mute"] = new ControlButton();
+            State.Controls["m"] = new ControlButton();
             State.Controls["share"] = new ControlButton();
+            State.Controls["mute"] = new ControlButton();
 
             State.Controls["motion"] = new ControlMotion();
 
@@ -264,8 +265,9 @@ namespace ExtendInput.Controller.Test
 
                                 (State.Controls["m1"   ] as IControlButton).DigitalStage1 = (reportData.ReportBytes[24] & 0x01) == 0x01;
                                 (State.Controls["m2"   ] as IControlButton).DigitalStage1 = (reportData.ReportBytes[24] & 0x02) == 0x02;
-                                (State.Controls["mute" ] as IControlButton).DigitalStage1 = (reportData.ReportBytes[24] & 0x40) == 0x40;
+                                (State.Controls["m"    ] as IControlButton).DigitalStage1 = (reportData.ReportBytes[24] & 0x10) == 0x10;
                                 (State.Controls["share"] as IControlButton).DigitalStage1 = (reportData.ReportBytes[24] & 0x20) == 0x20;
+                                (State.Controls["mute" ] as IControlButton).DigitalStage1 = (reportData.ReportBytes[24] & 0x40) == 0x40;
 
                                 (State.Controls["motion"] as ControlMotion).AngularVelocityX = BitConverter.ToInt16(reportData.ReportBytes, 28);
                                 (State.Controls["motion"] as ControlMotion).AngularVelocityZ = BitConverter.ToInt16(reportData.ReportBytes, 30);
